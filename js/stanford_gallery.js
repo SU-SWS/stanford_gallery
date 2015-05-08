b5e3dcbeec9293cf226f948b0d0a9bddacd3b40b
Shea McKinney <sherakama@gmail.com>
2015-05-08 08:30:27 -0700

    CAW-484: Updated JS to allow for multiple galleries on a page

--- js/stanford_gallery.js
+++ js/stanford_gallery.js
@@ -6,30 +6,35 @@ Drupal.behaviors.stanford_gallery = {
     // Define selector.
     var selector = "stanford-gallery-image";
 
-    // Start by adding the appropriate class to all of the items.
-    $(".field-name-field-s-gallery-image-info .field-items a", context).addClass(selector);
-
-    // Store the selected items for use.
-    var items = $("." + selector);
-
-    // If the captions setting is available set
-    if (Drupal.settings.stanford_gallery.captions.length) {
-      $.each(items, function(i, v) {
-        $(v).attr('title', Drupal.settings.stanford_gallery.captions[i]);
-      });
-    }
-
     // Default settings.
     var cboxsettings = {
-      rel : "group1",
+      rel : "group",
       scalePhotos : true,
       returnFocus : true,
       maxWidth : "98%",
       maxHeight : "90%"
     };
 
-    // Initialize the gallery.
-    items.colorbox(cboxsettings);
+    // Start by adding the appropriate class to all of the items.
+    $(".field-name-field-s-gallery-image-info .field-items a", context).addClass(selector);
+
+    // Store the selected items for use.
+    var items = $("." + selector);
+
+    // If the captions setting is available set it.
+    if (Drupal.settings.stanford_gallery.captions.length) {
+      $.each(items, function(i, v) {
+        $(v).attr('title', Drupal.settings.stanford_gallery.captions[i]);
+      });
+    }
+
+    // Init each gallery independantly
+    $.each($(".field-name-field-s-gallery-image-info"), function(i,v) {
+      var galleryItems = $(v).find("." + selector);
+      cboxsettings.rel = "group" + i;
+      // Initialize the gallery.
+      galleryItems.colorbox(cboxsettings);
+    });
 
   })(jQuery);
 
